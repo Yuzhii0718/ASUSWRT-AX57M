684ecda420f9057d5605687b549f8e4c80136cbe
Yuzhii0718 <yuzhii0718@outlook.com>
2026-07-26 14:20:39 +0800

    linux: add AN8811 PHY driver v1.3.3 from SDK

--- release/src-mtk-MT798X/linux/linux-5.4.x/drivers/net/phy/air_en8811h.h
+++ release/src-mtk-MT798X/linux/linux-5.4.x/drivers/net/phy/air_en8811h.h
@@ -1,35 +1,46 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*************************************************
+ * FILE NAME:  air_en8811h.h
+ * PURPOSE:
+ *      EN8811H PHY Driver for Linux
+ * NOTES:
+ *
+ *  Copyright (C) 2023 Airoha Technology Corp.
+ *************************************************/
 #ifndef __EN8811H_H
 #define __EN8811H_H
 
 #define EN8811H_MD32_DM             "EthMD32.dm.bin"
 #define EN8811H_MD32_DSP            "EthMD32.DSP.bin"
+#define EN8811H_IVY		            "ivypram.bin"
 
 #define EN8811H_PHY_ID1             0x03a2
 #define EN8811H_PHY_ID2             0xa411
 #define EN8811H_PHY_ID              ((EN8811H_PHY_ID1 << 16) | EN8811H_PHY_ID2)
 #define EN8811H_PHY_READY           0x02
-#define MAX_RETRY                   5
+#define EN8811H_PHY_IVY_READY		0xABC
+#define MAX_RETRY                   25
 
-#define EN8811H_TX_POLARITY_NORMAL   0x1
-#define EN8811H_TX_POLARITY_REVERSE  0x0
+#define EN8811H_TX_POL_NORMAL   0x1
+#define EN8811H_TX_POL_REVERSE  0x0
 
-#define EN8811H_RX_POLARITY_REVERSE  (0x1 << 1)
-#define EN8811H_RX_POLARITY_NORMAL   (0x0 << 1)
+#define EN8811H_RX_POL_NORMAL   (0x0 << 1)
+#define EN8811H_RX_POL_REVERSE  (0x1 << 1)
 
 
-/*
-The following led_cfg example is for reference only.
-LED0 Link 2500/Blink 2500 TxRx   (GPIO5)    <-> BASE_T_LED0,
-LED1 Link 1000/Blink 1000 TxRx   (GPIO4)    <-> BASE_T_LED1,
-LED2 Link 100 /Blink 100  TxRx   (GPIO3)    <-> BASE_T_LED2,
-*/
+/***************************************************************
+ * The following led_cfg example is for reference only.
+ * LED0 Link 2500/Blink 2500 TxRx   (GPIO5)    <-> BASE_T_LED0,
+ * LED1 Link 1000/Blink 1000 TxRx   (GPIO4)    <-> BASE_T_LED1,
+ * LED2 Link 100 /Blink 100  TxRx   (GPIO3)    <-> BASE_T_LED2,
+ ***************************************************************/
 /* User-defined.B */
-#define BASE_T_LED0_ON_CFG      (LED_ON_EVT_LINK_2500M)
-#define BASE_T_LED0_BLK_CFG     (LED_BLK_EVT_2500M_TX_ACT | LED_BLK_EVT_2500M_RX_ACT)
-#define BASE_T_LED1_ON_CFG      (LED_ON_EVT_LINK_1000M)
-#define BASE_T_LED1_BLK_CFG     (LED_BLK_EVT_1000M_TX_ACT | LED_BLK_EVT_1000M_RX_ACT)
-#define BASE_T_LED2_ON_CFG      (LED_ON_EVT_LINK_100M)
-#define BASE_T_LED2_BLK_CFG     (LED_BLK_EVT_100M_TX_ACT | LED_BLK_EVT_100M_RX_ACT)
+#define AIR_LED0_ON     (LED_ON_EVT_LINK_2500M)
+#define AIR_LED0_BLK    (LED_BLK_EVT_2500M_TX_ACT | LED_BLK_EVT_2500M_RX_ACT)
+#define AIR_LED1_ON     (LED_ON_EVT_LINK_1000M)
+#define AIR_LED1_BLK    (LED_BLK_EVT_1000M_TX_ACT | LED_BLK_EVT_1000M_RX_ACT)
+#define AIR_LED2_ON     (LED_ON_EVT_LINK_100M)
+#define AIR_LED2_BLK    (LED_BLK_EVT_100M_TX_ACT | LED_BLK_EVT_100M_RX_ACT)
 /* User-defined.E */
 
 /* CL45 MDIO control */
@@ -37,7 +48,7 @@ LED2 Link 100 /Blink 100  TxRx   (GPIO3)    <-> BASE_T_LED2,
 #define MII_MMD_ADDR_DATA_REG       0x0e
 #define MMD_OP_MODE_DATA            BIT(14)
 
-#define EN8811H_FW_VERSION          "1.1.3"
+#define EN8811H_DRIVER_VERSION      "v1.3.3"
 
 #define LED_ON_CTRL(i)              (0x024 + ((i)*2))
 #define LED_ON_EN                   (1 << 15)
@@ -62,9 +73,6 @@ LED2 Link 100 /Blink 100  TxRx   (GPIO3)    <-> BASE_T_LED2,
 #define LED_BLK_EVT_1000M_RX_ACT    (1 << 1)
 #define LED_BLK_EVT_1000M_TX_ACT    (1 << 0)
 /* LED Blinking Event Option.E*/
-#define LED_ENABLE                  1
-#define LED_DISABLE                 0
-
 #define EN8811H_LED_COUNT           3
 
 #define LED_BCR                     (0x021)
@@ -80,72 +88,126 @@ LED2 Link 100 /Blink 100  TxRx   (GPIO3)    <-> BASE_T_LED2,
 #define LED_BLK_DUR                 (0x023)
 #define LED_BLK_DUR_MASK            (0xffff)
 
-#define UNIT_LED_BLINK_DURATION     1024
+#define UNIT_LED_BLINK_DURATION     781
 
-#define AIR_RTN_ON_ERR(cond, err)  \
-    do { if ((cond)) return (err); } while(0)
-
-#define AIR_RTN_ERR(err)            AIR_RTN_ON_ERR(err < 0, err)
-
-#define LED_SET_EVT(reg, cod, result, bit) do         \
-    {                                                 \
-        if(reg & cod) {                               \
-            result |= bit;                            \
-        }                                             \
-    } while(0)
-
-#define LED_SET_GPIO_SEL(gpio, led, val) do           \
-    {                                                 \
-        val |= (led << (8 * (gpio % 4)));         \
-    } while(0)
+#define GET_BIT(val, bit) ((val & BIT(bit)) >> bit)
 
 #define INVALID_DATA                0xffff
 #define PBUS_INVALID_DATA           0xffffffff
 
-typedef struct AIR_BASE_T_LED_CFG_S
-{
-    u16 en;
-    u16 gpio;
-    u16 pol;
-    u16 on_cfg;
-    u16 blk_cfg;
-}AIR_BASE_T_LED_CFG_T;
-typedef enum
-{
-    AIR_LED2_GPIO3 = 3,
-    AIR_LED1_GPIO4,
-    AIR_LED0_GPIO5,
-    AIR_LED_LAST
-} AIR_LED_GPIO;
+/* MII Registers */
+#define AIR_AUX_CTRL_STATUS		0x1d
+#define AIR_AUX_CTRL_STATUS_SPEED_MASK	GENMASK(4, 2)
+#define AIR_AUX_CTRL_STATUS_SPEED_100		0x4
+#define AIR_AUX_CTRL_STATUS_SPEED_1000	0x8
+#define AIR_AUX_CTRL_STATUS_SPEED_2500	0xc
 
-typedef enum {
-    AIR_BASE_T_LED0,
-    AIR_BASE_T_LED1,
-    AIR_BASE_T_LED2,
-    AIR_BASE_T_LED3
-}AIR_BASE_T_LED;
+/* Registers on BUCKPBUS */
+#define EN8811H_2P5G_LPA		0x3b30
+#define EN8811H_2P5G_LPA_2P5G			BIT(0)
 
-typedef enum
-{
-    AIR_LED_BLK_DUR_32M,
-    AIR_LED_BLK_DUR_64M,
-    AIR_LED_BLK_DUR_128M,
-    AIR_LED_BLK_DUR_256M,
-    AIR_LED_BLK_DUR_512M,
-    AIR_LED_BLK_DUR_1024M,
-    AIR_LED_BLK_DUR_LAST
-} AIR_LED_BLK_DUT_T;
+#define EN8811H_FW_CTRL_1		0x0f0018
+#define   EN8811H_FW_CTRL_1_START		0x0
+#define   EN8811H_FW_CTRL_1_FINISH		0x1
+#define EN8811H_FW_CTRL_2		0x800000
+#define EN8811H_FW_CTRL_2_LOADING		BIT(11)
+#define EN8811H_LOOP      0x800
 
-typedef enum
-{
-    AIR_ACTIVE_LOW,
-    AIR_ACTIVE_HIGH,
-} AIR_LED_POLARITY;
-typedef enum
-{
-    AIR_LED_MODE_DISABLE,
-    AIR_LED_MODE_USER_DEFINE,
-    AIR_LED_MODE_LAST
-} AIR_LED_MODE_T;
+#define EN8811H_HWTRAP1     0xcf914
+#define EN8811H_HWTRAP1_CKO     BIT(12)
 
-#endif /* End of __EN8811H_MD32_H */
+#define EN8811H_CLK_CGM     0xcf958
+#define EN8811H_CLK_CGM_CKO     BIT(26)
+
+#define NUM_ASI_REGS       5
+struct air_cable_test_rsl {
+	int          status[4];
+	unsigned int length[4];
+};
+
+struct air_base_t_led_cfg {
+	u16 en;
+	u16 gpio;
+	u16 pol;
+	u16 on_cfg;
+	u16 blk_cfg;
+};
+
+struct en8811h_priv {
+	struct dentry       *debugfs_root;
+	unsigned int        dm_crc32;
+	unsigned int        dsp_crc32;
+	unsigned int        ivy_crc32;
+	int                 pol;
+	int                 surge;
+	int                 cko;
+	struct kobject      *cable_kobj;
+	int                 running_status;
+	int                 pair[4];
+	int                 an;
+	int                 link;
+	int                 speed;
+	int                 duplex;
+	int                 pause;
+	int                 asym_pause;
+	u16                 on_crtl[3];
+	u16                 blk_crtl[3];
+	u32                 firmware_version;
+	bool                mcu_needs_restart;
+	bool                mcu_load;
+	int                 debug;
+	int                 phy_handle;
+	int                 init_stage;
+	int                 need_an;
+	int                 count;
+	struct air_base_t_led_cfg led[3];
+	int                 duration;
+	int                 led_dts;
+};
+
+enum air_init_stage {
+	AIR_INIT_START,
+	AIR_INIT_CONFIG,
+	AIR_INIT_FW_LOADING,
+	AIR_INIT_FW_READY,
+	AIR_INIT_SUCESS,
+	AIR_INIT_FW_FAIL,
+	AIR_INIT_FAIL,
+	AIR_INIT_LAST
+};
+
+enum air_led_gpio {
+	AIR_LED2_GPIO3 = 3,
+	AIR_LED1_GPIO4,
+	AIR_LED0_GPIO5,
+	AIR_LED_LAST
+};
+
+enum air_base_t_led {
+	AIR_BASE_T_LED0,
+	AIR_BASE_T_LED1,
+	AIR_BASE_T_LED2,
+	AIR_BASE_T_LED3
+};
+
+enum air_led_blk_dur {
+	AIR_LED_BLK_DUR_32M,
+	AIR_LED_BLK_DUR_64M,
+	AIR_LED_BLK_DUR_128M,
+	AIR_LED_BLK_DUR_256M,
+	AIR_LED_BLK_DUR_512M,
+	AIR_LED_BLK_DUR_1024M,
+	AIR_LED_BLK_DUR_LAST
+};
+
+enum air_led_polarity {
+	AIR_ACTIVE_HIGH,
+	AIR_ACTIVE_LOW,
+};
+enum air_led_mode {
+	AIR_LED_MODE_DISABLE,
+	AIR_LED_MODE_USER_DEFINE,
+	AIR_LED_MODE_LAST
+};
+
+#endif /* End of __EN8811H_H */
